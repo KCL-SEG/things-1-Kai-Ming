469860ae5d568b5344d673a891dcb91dee68e596
Kai Ming Tey <kai.tey@kcl.ac.uk>
2022-10-09 17:08:12 +0100

    Fixed compilation error

--- things/models.py
+++ things/models.py
@@ -1,14 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Thing:
+class Thing(AbstractUser):
     def __init__(self, name, description, quantity):
         self.name = name
         self.description = description
         self.quantity = quantity
 
-    def __del__(self):
-        print('A thing was deleted')
+        
 
     def full_clean(self):
         del(self)
