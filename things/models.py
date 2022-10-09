6f1b0e2ad5dce8c5be55e4d041fe7f31bb64e5c9
Kai Ming Tey <kai.tey@kcl.ac.uk>
2022-10-09 17:11:37 +0100

    Fixed compilation error

--- things/models.py
+++ things/models.py
@@ -3,12 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Thing(AbstractUser):
+    """
     def __init__(self, name, description, quantity):
         self.name = name
         self.description = description
         self.quantity = quantity
-
+"""
         
-
-    def full_clean(self):
-        del(self)
