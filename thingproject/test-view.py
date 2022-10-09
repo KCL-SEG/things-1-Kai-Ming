ed71bd8a355c3953969c77795ea97c5fe1d50313
Kai Ming Tey <kai.tey@kcl.ac.uk>
2022-10-09 17:45:06 +0100

    Fixed compilation error

--- thingproject/test-view.py
+++ thingproject/test-view.py
@@ -10,5 +10,6 @@ class ViewTest(TestCase):
 
     def test_content(self):
         response = self.client.get('/')
+        print(response.content)
         self.assertContains(response, '<title>Things</title>', html=True)
         self.assertContains(response, '<h1>Things</h1>', html=True)
