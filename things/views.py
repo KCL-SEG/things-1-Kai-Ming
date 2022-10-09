ec59857abe69ba7a368f4b5138eae31004fead31
Kai Ming Tey <kai.tey@kcl.ac.uk>
2022-10-09 17:26:03 +0100

    Fixed compilation error

--- things/views.py
+++ things/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    response = HttpResponse('<html>Things</html><h1>Things</h1>')
+    response = HttpResponse('<html>Things</html>')
     return response
     #return render(request, 'home.html')
