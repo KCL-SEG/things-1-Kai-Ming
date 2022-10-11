54abf95fe35ff0cf82c57f8d3af012d155d837c5
Kai Ming Tey <kai.tey@kcl.ac.uk>
2022-10-11 13:17:37 +0100

    Started with model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
+"""
 class Thing(models):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     quantity = models.IntegerField()       
+"""
