afbe46cfdf184fdcc345f3866cf689c501f24ad1
Kai Ming Tey <kai.tey@kcl.ac.uk>
2022-10-11 14:00:06 +0100

    Models

--- things/models.py
+++ things/models.py
@@ -1,8 +1,22 @@
+from enum import unique
+from pickle import FALSE
 from django.db import models
 
 # Create your models here.
 
 class Thing(models.Model):
-    name = models.CharField(max_length=20)
-    description = models.CharField(max_length=100)
-    quantity = models.IntegerField()       
+    name = models.CharField(
+        max_length=30,
+        unique = True,
+        blank = True,
+    )
+    description = models.CharField(
+        max_length=120,
+        blank = False,
+        unique = False,
+    )
+    quantity = models.IntegerField(
+        min_value=0,
+        max_value=100,
+
+    )       
